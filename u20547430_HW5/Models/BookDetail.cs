868507bdae3ef8a21d0a540ca8d1905ca85d0d46
20547430 <u20547430@tuks.co.za>
2022-09-27 15:26:03 +0200

    BookDetails Model

---
+++ u20547430_HW5/Models/BookDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20547430_HW5.Models
+{
+    public class BookDetail
+    {
+        public int BookID { get; set; }
+        public DateTime TakenDate { get; set; }
+        public DateTime BroughtDate { get; set; }
+
+        public string Borrowedby { get; set; }
+
+    }
+}
